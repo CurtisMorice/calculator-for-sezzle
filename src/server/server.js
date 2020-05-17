501120977d1955ca554b18ec99aa6139bf872075
CurtisM <curtismorice@gmail.com>
2020-05-17 10:12:50 -0500

    removed logos and removed hrefs foor favicon added cleardb to work with mysql

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const path = require('path')
 require('dotenv').config();
 
 const app = express();
